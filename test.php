7b4bb244c2ef70656a64259ef62a45e0c22d4200
JulianFun123 <julianfun123@yahoo.com>
2019-11-17 23:12:34 +0100

    Hello

--- test.php
+++ test.php
@@ -4,9 +4,10 @@ require "Git.php";
 use modules\git\Git;
 
 $git = new Git();
+$git->changeDirectory("test");
 $git->initIfNot();
 
-$git->addRemote("origin", "git@github.com:interaapps/javahttprequest.git");
+// $git->addRemote("origin", "git@github.com:interaapps/javahttprequest.git");
 
 $git->setRemote("origin");
 
